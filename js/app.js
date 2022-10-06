2b435b36493b404edbac08fafecb58c51327f7e1
gyungsubLee <gyungsub@DESKTOP-1JB0K0U.localdomain>
2022-10-06 19:28:46 +0900

    feat : Add 세부 ablum 화면에서 배경 클릭 시 main으로 전환

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 const albumContainer = document.querySelector('.album-container');
 const sliderContainer = document.querySelector('.album-slider-container');
+const body = document.querySelector('body');
 const SToABnts = document.getElementsByClassName('slider-to-album');
 
-const handleSlinderHidden = ()=> {
+
+const AddSlinderHidden = ()=> {
     setTimeout(()=>{
         albumContainer.classList.remove("hidden");
         sliderContainer.classList.add("hidden");
     }, 400)
 }
 
-Array.from(SToABnts).forEach(Bnt => Bnt.addEventListener('click', handleSlinderHidden));
+Array.from(SToABnts).forEach(Bnt => Bnt.addEventListener('click', AddSlinderHidden));
+
+
+window.addEventListener('click', (e) => {
+    if(sliderContainer.classList.contains("hidden") && e.target === body) {
+        albumContainer.classList.add("hidden");
+        sliderContainer.classList.remove("hidden");
+    }
+});
